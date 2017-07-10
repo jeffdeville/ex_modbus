d7f9acf2d981f62bdea88af5587f489b5c4c41cd
Jeff Deville <jeffdeville@gmail.com>
2017-07-10 13:43:20 -0400

    force big endianness on all binary type mappings

--- lib/ex_modbus/types.ex
+++ lib/ex_modbus/types.ex
@@ -2,16 +2,16 @@ defmodule ExModbus.Types do
   use Bitwise
 
   def from_binary(<<127, 192, _rest::binary>>, _),                       do: {:ok, nil}
-  def from_binary(<<bitfield::unsigned-integer-size(32)>>, :bitfield32), do: {:ok, bitfield}
-  def from_binary(<<bitfield::unsigned-integer-size(16)>>, :bitfield16), do: {:ok, bitfield}
-  def from_binary(<<int::signed-integer-size(16)>>,        :int16),      do: {:ok, int}
-  def from_binary(<<uint::unsigned-integer-size(16)>>,     :enum16),     do: {:ok, uint}
-  def from_binary(<<uint::unsigned-integer-size(16)>>,     :uint16),     do: {:ok, uint}
-  def from_binary(<<uint::unsigned-integer-size(32)>>,     :uint32),     do: {:ok, uint}
-  def from_binary(<<sf::signed-integer-size(16)>>,         :sunssf),     do: {:ok, sf}
-  def from_binary(<<sf::signed-integer-size(32)>>,         :sunssf),     do: {:ok, sf}
-  def from_binary(<<flt::float-size(32)>>,                 :float32),    do: {:ok, flt}
-  def from_binary(<<uint::unsigned-integer-size(32)>>,     :acc32),      do: {:ok, uint}
+  def from_binary(<<bitfield::unsigned-integer-size(32)-big>>, :bitfield32), do: {:ok, bitfield}
+  def from_binary(<<bitfield::unsigned-integer-size(16)-big>>, :bitfield16), do: {:ok, bitfield}
+  def from_binary(<<int::signed-integer-size(16)-big>>,        :int16),      do: {:ok, int}
+  def from_binary(<<uint::unsigned-integer-size(16)-big>>,     :enum16),     do: {:ok, uint}
+  def from_binary(<<uint::unsigned-integer-size(16)-big>>,     :uint16),     do: {:ok, uint}
+  def from_binary(<<uint::unsigned-integer-size(32)-big>>,     :uint32),     do: {:ok, uint}
+  def from_binary(<<sf::signed-integer-size(16)-big>>,         :sunssf),     do: {:ok, sf}
+  def from_binary(<<sf::signed-integer-size(32)-big>>,         :sunssf),     do: {:ok, sf}
+  def from_binary(<<flt::float-size(32)-big>>,                 :float32),    do: {:ok, flt}
+  def from_binary(<<uint::unsigned-integer-size(32)-big>>,     :acc32),      do: {:ok, uint}
   def from_binary(data, :string32), do: from_binary(data, :string)
   def from_binary(data, :string16), do: from_binary(data, :string)
   def from_binary(data, <<"String", _size::binary>>), do: from_binary(data, :string)
@@ -26,19 +26,19 @@ defmodule ExModbus.Types do
   def from_binary(data, type), do: {:type_conversion_error, {data, type}}
 
 
-  def to_binary(data, :int16, _), do: {:ok, <<data::signed-integer-size(16)>>}
-  def to_binary(data, :uint16, _) when data >= 0, do: {:ok, <<data::unsigned-integer-size(16)>>}
+  def to_binary(data, :int16, _), do: {:ok, <<data::signed-integer-size(16)-big>>}
+  def to_binary(data, :uint16, _) when data >= 0, do: {:ok, <<data::unsigned-integer-size(16)-big>>}
   def to_binary(data, :uint16, _) when data < 0, do: {:invalid_data_type, "Data type is unsigned. No negative numbers"}
   def to_binary(data, :enum16, enum_map) when is_integer(data) do
     case Map.has_key?(enum_map, data) do
-      true -> {:ok, <<data::unsigned-integer-size(16)>>}
+      true -> {:ok, <<data::unsigned-integer-size(16)-big>>}
       false -> {:invalid_enum_value, "#{data} is not a valid value in the enum: #{inspect enum_map}"}
     end
   end
   def to_binary(data, :enum16, enum_map) when is_binary(data) do
     flipped_map = enum_map |> flip_keys
     case Map.has_key?(flipped_map, data) do
-      true -> {:ok, <<Map.fetch!(flipped_map, data)::unsigned-integer-size(16)>>}
+      true -> {:ok, <<Map.fetch!(flipped_map, data)::unsigned-integer-size(16)-big>>}
       false -> {:invalid_enum_value, "#{data} is not a valid value in the enum: #{inspect flipped_map}"}
     end
   end
